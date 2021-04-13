b9f115daf320c622f8d986caf5997eb06b2e713c
svetlana.kim <svetlana.kim.7008@gmail.com>
2021-04-13 23:51:34 +0700

    добавила маски для инпутов, стили

--- index.js
+++ index.js
@@ -1,295 +1,39 @@
-let jsonAddPost = `{
-    "name":"addpost",
-    "fields":[
-    {
-        "label":"Title",
-        "input": {
-            "type":"text",
-            "required": true
-        }
-    },
-    {
-        "label":"Description",
-        "input": {
-            "type":"textarea",
-            "required":true
-        }
-    },
-    {
-        "label":"Image",
-        "input": {
-            "type":"file",
-            "required": true
-        }
-    },
-    {
-        "label":"Publish Date",
-        "input": {
-            "type": "date",
-            "required": true
-        }
-    },
-    {
-        "label": "Author",
-        "input": {
-            "type": "text"
-        }
-    }
-],
-"references":[
-    {
-      "input":{
-        "type":"checkbox",
-        "required":true,
-        "checked":"false"
-      }
-    },
-    {
-        "text without ref":"View Author Post",
-        "text":"View Author Post",
-        "ref":"viewauthor"
-    }
-  ],
-    "buttons":[
-    {
-        "text":"Create Post"
-    }
-]
-}`;
-
-let jsonInterview = `{
-    "name":"interview",
-    "fields":[
-        {
-            "label":"Введите своё ФИО",
-            "input":{
-                "type":"text",
-                "required":true,
-                "placeholder":"Иванов Иван Иванович"
-            }
-        },
-        {
-          "label":"Введите Номер телефона",
-          "input":{
-              "type":"number",
-              "required":true,
-              "mask": "+7 (999) 99-99-999"
-          }
-        },
-        {
-          "label":"Введите свою Почту",
-          "input":{
-              "type":"email",
-              "required":true,
-              "placeholder":"example@mail.com"
-          }
-        },
-        {
-            "label":"Введите свой возраст",
-            "input":{
-                "type":"number",
-                "required":true
-            }
-        },
-        {
-            "label":"Введите вашу специальность",
-            "input":{
-                "type":"text",
-                "required":true
-            }
-        },
-        {
-            "label":"Выберете технологии, с которыми вы работали",
-            "input":{
-                "type":"technology",
-                "required": true,
-                "technologies": ["PHP", "JS", "Laravel", "Express.js", "Yii2", "HTML", "CSS", "Java"],
-                "multiple": true
-            }
-        },
-        {
-            "label":"Ваш срок работы",
-            "input":{
-                "type":"number",
-                "required": true
-            }
-        },
-        {
-            "label":"Ваша фотография",
-            "input":{
-                "type":"file",
-                "required":true
-            }
-        },
-        {
-            "label":"Серия, номер",
-            "input":{
-                "type": "number",
-                "required": true,
-                "mask": "99-99 999999"
-            }
-        },
-        {
-            "label":"Код подразделения",
-            "input":{
-                "type": "number",
-                "required": true,
-                "mask": "999-999"
-            }
-        },
-        {
-            "label":"Скан / Фото паспорта (1 страница)",
-            "input":{
-                "type": "file",
-                "required": true,
-                "multiple": true,
-                "filetype": ["png", "jpeg", "pdf"]
-            }
-        },
-        {
-            "label":"Расскажите немного о себе",
-            "input":{
-                "type":"textarea",
-                "required:":true
-            }
-        }
-    ],
-    "references":[
-        {
-          "input":{
-            "type":"checkbox",
-            "required":true,
-            "checked":"false"
-          }
-        },
-        {
-            "text without ref":"I accept the",
-            "text":"Terms & Conditions",
-            "ref":"termsandconditions"
-        }
-    ],
-    "buttons":[
-        {
-            "text":"Send"
-        },
-        {
-            "text":"Cancel"
-        }
-    ]
-}`;
-
-let jsonSignin = `{
-    "name":"login",
-    "fields":[
-        {
-            "label":"Enter your login or email",
-            "input":{
-                "type":"text",
-                "required":true,
-                "placeholder": "login or email"
-            }
-        },
-        {
-            "label":"Enter your password",
-            "input":{
-                "type":"password",
-                "required":true,
-                "placeholder": "password"
-            }
-        }
-    ],
-    "references":[
-        {
-            "text":"Forgot password?",
-            "ref":"rememberpassword"
-        },
-        {
-            "text":"Create new account",
-            "ref":"signup"
-        }
-    ],
-    "buttons":[
-        {
-            "text":"Login"
-        }
-    ]
-}`;
-
-let jsonSignup = `{
-    "name":"register",
-    "fields":[
-    {
-        "input":{
-            "type":"text",
-            "required":true,
-            "placeholder":"Enter full name"
-        }
-    },
-    {
-        "input":{
-            "type":"email",
-            "required":true,
-            "placeholder":"Enter email"
-        }
-    },
-    {
-        "input":{
-            "type":"password",
-            "required":true,
-            "placeholder":"password"
-        }
-    },
-    {
-        "input":{
-            "type":"password",
-            "required":true,
-            "placeholder":"Confirm password"
-        }
-    }
-    ],
-    "references":[
-        {
-            "text without ref":"Already have account?",
-            "text":"Login",
-            "ref":"signin"
-        }
-    ],
-    "buttons":[
-    {
-        "text":"Sign Up"
-    }
-]
-}`;
-
-let jsonColorSheme = `{
-    "name":"website_color_scheme",
-    "fields":[
-        {
-            "label":"Choose color scheme",
-            "input":{
-                "type":"color",
-                "colors":["#3366ff","#009933","#990033","#996633"]
-            }
-        },
-        {
-            "input":{
-                "type":"checkbox",
-                "checked":"false"
-            },
-            "label":"Turn on dark theme?"
-        }
-    ]
-}`
-
 let addFormFields = function (elements) {
     elements.forEach(el => {
+
         let label = el.label ? `<label>${el.label}</label>` : '';
         let required = el.input.required ? 'required' : '';
         let checked = el.input.checked ? 'checked' : '';
         let placeholder = el.input.placeholder ? `placeholder="${el.input.placeholder}"` : '';
+        let mask = el.input.mask ? `data-inputmask="'mask': '${el.input.mask}'"` : '';
+        if (mask){
+            el.input.type = 'text';
+        }
+        
+        let multiple;
+        let accept;
+        if(el.input.type === "file"){
+            multiple = el.input.multiple ? 'multiple' : '';
+            accept = el.input.filetype ? `accept="${el.input.filetype}"` : ''; 
+        }
+
+        let str = `<input class ='form-control' type="${el.input.type}" ${required} ${placeholder} ${checked} ${mask} ${multiple} ${accept}>`;
+
+        if(el.input.technologies){
+            let valuesMultiple = el.input.technologies;
+            str = '';
+            for(i = 0; i < valuesMultiple.length; i++){
+                str += `<div class="form-check">
+                <input class="form-check-input" type="checkbox" value="" id="Check${i}">
+                <label class="form-check-label" for="Check${i}">
+                ${valuesMultiple[i]}
+                </label>
+                </div>`
+            }
+        }
         $('form').append(`<div class='wrapper'>
                             ${label}
-                            <input class ='form-control' type="${el.input.type}"  ${required} ${placeholder} ${checked}>
+                            ${str}
                          </div>`)
     })
 }
@@ -307,13 +51,13 @@ let addFormReferences = function (elements) {
             let label = el.label ? `<label>${el.label}</label>` : '';
             let required = el.input.required ? 'required' : '';
             let checked = el.input.required ? 'checked' : '';
-            // let textBeforLink = el["text without ref"] ? `${el["text without ref"]` :"";
             $('form').append(`<div class='wrapper'>
-                        <input class ='' type="${el.input.type}"  ${required} ${checked}>
-                        ${label}
-                        </div>`)
+            <input class ='' type="${el.input.type}"  ${required} ${checked}>
+            ${label}
+            </div>`)
         } else {
-            $('form').append(`<div>${el["text without ref"]}
+            let text = el["text without ref"] ? `${el["text without ref"]}` : '';
+            $('form').append(`<div>${text}
                               <a href="${el.ref}">${el.text}</a>
                           </div>`);
         }
@@ -332,25 +76,29 @@ let addFormFunction = function(obj) {
     }
 }
 
-
-let myForm;
+let fileContent;
 function processFiles(files) {
     var file = files[0];
+    if(!file){
+        return;
+    }
     var reader = new FileReader();
     reader.onload = function (e) {
         // Когда это событие активируется, данные готовы.        
-        myForm = e.target.result;
+        fileContent = e.target.result;
     };
-    reader.readAsText(file);    
+    reader.readAsText(file);  
 }
 
 $(document).ready(function () {    
     $( ".add-file" ).click(function() {
         // console.log(`Hi + ${myForm}`);        
-        myForm = JSON.parse(myForm);
-        addFormFunction(myForm);
+        fileContent = JSON.parse(fileContent);
+        $('form').html('');
+        addFormFunction(fileContent);
+        $(":input").inputmask();
     });
-
+    
     $( ".clear-form" ).click(function() {
         $('form').html('');
     });
